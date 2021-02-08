a698b3629830e3ec372d981bdcf3630546490b58
leelin <liac512@outlook.com>
2021-02-08 23:10:04 +0800

    day1 use make to compile and py to diff

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 一些突发奇想的小型内容。
 
 ### Diff
-TODO
+#### 利用Makefile 来编译
+1. 教程连接：[第三方教程](https://makefiletutorial.com/#makefile-cookbook),[GNU官方](https://www.gnu.org/software/make/manual/make.html)
  
---
+++ diff/diff.py
@@ -0,0 +1,14 @@
+import re
+
+
+class Project:
+
+    def __init__(self, pathname):
+        if (pathname):
+            this.pathname = pathname
+            this.filename = pathname.split('/')[-1]
+        else 
+            raise()
+
+    
+    def diff(self, )
